d13d950975f4862fa60daf6a52eeb7dfad578a47
unknown <amazighmalek.com>
2024-05-05 19:10:58 +0200

    Image Component

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,14 +1,38 @@
-import Link from 'next/link'
-import React from 'react'
 
+import Image from 'next/image'
+import CloudImage from '../../../public/cloud-hosting.png'
 const AboutPage = () => {
   return (
-    <div className='flex flex-col items-center justify-center h-screen'>
-      About
-      <button className='bg-black text-white rounded p-1'>
-      <Link href={'/'}>Back to home</Link>
-      </button>
-    </div>
+   <section className='fix-height m-auto container'>
+    <h1 
+  className='text-center my-3 text-3xl font-semibold text-blue-950 '>
+      Aboute Page</h1>
+      <p className="py-4 my-4">
+      We can't solve every use case, but in the spirit 
+      of create-react-app, we can try to provide some 
+      tools that abstract over the setup process and 
+      handle the most common use cases, as well as 
+      include some useful utilities that will let 
+      the user simplify their application code.
+    Redux Toolkit also includes a powerful data fetching 
+    and caching capability that we've dubbed "RTK Query". 
+    It's included in the package as a separate set of entry 
+     points. It's optional, but can eliminate the need 
+       to hand-write data fetching logic your
+     These tools should be beneficial to all Redux users. 
+     Whether you're a brand new Redux user setting up your 
+     first project, or an experienced user who wants 
+      to simplify an existing application, Redux Toolkit 
+      can help you make your Redux code better
+      </p>
+
+      <div>
+        <Image src={CloudImage} 
+        height={500} width={500}
+        priority={true} alt='y' />
+      </div>
+
+   </section>
   )
 }
 
